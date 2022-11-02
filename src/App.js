5225cd65463d775c8ba8fcf79e51e37209a18163
rajibmalik <rajibrmalik@gmail.com>
2022-11-02 21:49:19 +0000

    Able to print todos to the browser

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React, { useState, useRef } from 'react'; // uuse of useState hook built into React
+import React, { useState} from 'react'; // hooks built into React
 import TodoList from './TodoList'
 
 function App() {
-  const [todos, setTodos] = useState([]) // Object de-structuring
-  const todoNameRef = useRef()
-
-  const handleAddTodo  = (e) => {
-    const name =todoNameRef.current.values
-    if (name === "") return 
-    console.log(name)
-    setTodos(prevTodos => {
-      return [...prevTodos, {id: 1, name: name, complete: false}]
-      })
-    todoNameRef.current.value = null // This clears the input box each time the button is clicked
-
-  }
-
-  return (                               // Reacts version of html JSX 
-    <>                           {/* Fragmentation allows us to put two html/JSX elements in a return*/}    
-     <TodoList todos={todos} />  {/* 'todos' is a 'prop' being passed the 'todos' variable */}
-     <input ref={todoNameRef} type="text"  />      {/* Usually cansnot put two html/JSX elements in a return */}  
-     <button onClick={handleAddTodo}>Add A Todo</button>  
-     <button>Clear Completed Todos</button>
-     <div>0 left to do</div>
-    </>       
+  const [todos, setTodos] = useState(['Todo 1']) // Object de-structuring
+  return ( // JSX React's version of html
+    <>
+   <TodoList todos={todos} />   {/* 'todos' is a prop being passes the todos varibale in our use state */}
+    <input type="text" />
+    <button>Add a Todo</button>
+    <button>Clear Completed Todos</button>
+    <div>0 left to do</div>
+    </>
   )
-};
+}
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import Todo from './Todo'
 
-export default function TodoList({ todos }) { // We are passing in the todos list variable from App.js
+export default function TodoList({todos}) {
   return (
-    todos.map(todo => {
-      return <Todo key={todo.id} todo={todo} /> // We are passing in the todo variable from Todo.js which we are importing
-                                             // We only want React to render the Todos that change so we give it key
-                                             // the todo id is going to be unique, we need the key to be unique
-                       
-    })
+   todos.map(todo => {
+    return <Todo todo={todo} /> // Todo is imported from Todo
+   })
   )
 }
-
-
-
